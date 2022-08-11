bd888b3dcbdc00896a08b9833b4b2a9e924674b5
Julia Capelasso <102771898+juliacapelasso@users.noreply.github.com>
2022-08-11 12:06:48 -0300

    Update Step 2 - Production database - 08.sql

--- Step 2 - Production database - 08.sql
+++ Step 2 - Production database - 08.sql
@@ -2,7 +2,11 @@ select * from [dbo].[product_category_name_translation]
 create table db_act_CATEGORY
 (
 product_category_name nvarchar (150),
-product_category_name_english nvarchar (50),
-
+product_category_name_english nvarchar (50)
 
 )
+
+insert into db_act_CATEGORY
+select * from [dbo].[product_category_name_translation]
+
+select * from [dbo].[db_act_CATEGORY] 
