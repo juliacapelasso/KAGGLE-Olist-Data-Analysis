edf073fe0211044aebba5211e62c95542990c1f4
Julia Capelasso <102771898+juliacapelasso@users.noreply.github.com>
2022-08-11 11:59:52 -0300

    Update Step 2 - Production database - 07.sql

--- Step 2 - Production database - 07.sql
+++ Step 2 - Production database - 07.sql
@@ -6,5 +6,9 @@ seller_zip_code_prefix nvarchar (50),
 seller_city nvarchar (50),
 seller_state nvarchar (2),
 
-
 )
+
+insert into db_act_SELLERS
+select * from [dbo].[olist_sellers_dataset]
+
+select * from [dbo].[db_act_SELLERS]
