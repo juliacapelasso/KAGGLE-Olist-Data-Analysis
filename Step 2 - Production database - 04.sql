6ffffa83926fe512acf543fc32acd90e4209a0d2
Julia Capelasso <102771898+juliacapelasso@users.noreply.github.com>
2022-08-11 11:54:31 -0300

    Update Step 2 - Production database - 04.sql

--- Step 2 - Production database - 04.sql
+++ Step 2 - Production database - 04.sql
@@ -7,5 +7,10 @@ payment_type nvarchar (50),
 payment_installments nvarchar (2),
 payment_value float
 
-
 )
+
+insert into db_act_PAYMENTS
+select * from [dbo].[olist_order_payments_dataset]
+
+select * from [dbo].[db_act_CUSTOMER]
+
